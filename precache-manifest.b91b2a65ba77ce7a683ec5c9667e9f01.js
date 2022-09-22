ad59a4659702fedfe2bfa84cfd9809991e6cfa2d
Pedro Costa dos Santos <pedro.santos405@outlook.com>
2022-09-21 23:47:14 -0300

    Updates

---
+++ precache-manifest.b91b2a65ba77ce7a683ec5c9667e9f01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8a4087c25b9af7f731c8604038b418",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "1b28dea52f413758a2a1",
+    "url": "/trivia/static/css/main.61b300ab.chunk.css"
+  },
+  {
+    "revision": "8a4c0a65cb21f88ce20f",
+    "url": "/trivia/static/js/2.d92eff98.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trivia/static/js/2.d92eff98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b28dea52f413758a2a1",
+    "url": "/trivia/static/js/main.e04163ef.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/trivia/static/js/main.e04163ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526b50ebb9f7d5ea338f",
+    "url": "/trivia/static/js/runtime-main.f849f328.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
